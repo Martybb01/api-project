dd062263f63ac15375aa9c391d6131fcf2768268
Martib01 <martybb01@gmail.com>
2022-09-13 17:03:26 +0200

    split up the server to make it testable

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,4 @@
-import express from "express";
-import "express-async-errors"; //make sure the server is stable with no issues or crash when using async/await
-
-const app = express();
-
-// route handler
-app.get("/", (request, response) => {
-    response.send("This is the Space Facts API!");
-});
+import app from "./app";
 
 // listen
 const port = 3000;
