05468882552d433d246cdb6b3f01d2600c9ddb9b
Martib01 <martybb01@gmail.com>
2022-09-16 18:40:10 +0200

    add post planet route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import "express-async-errors"; //make sure the server is stable with no issues or crash when using async/await
 import prisma from "./lib / prisma/client";
+
 const app = express();
+
+// built-in middleware that take the incoming json and parse it in an object
+app.use(express.json());
 
 // GET /planets - Retrieve all planets
 app.get("/planets", async (request, response) => {
     const planets = await prisma.planet.findMany();
 
     response.json(planets);
+});
+
+// POST - create a new planet
+app.post("/planets", async (request, response) => {
+    const planet = request.body;
+
+    response.status(201).json(planet);
 });
 
 export default app;
